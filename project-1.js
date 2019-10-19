6cc2f6310fe0574adb8f3fe49d026dbbef2e9de6
Gabriel Mixon <gabrielmixon@gmail.com>
2019-10-19 14:03:53 -0400

    screw cors

--- project-1.js
+++ project-1.js
@@ -1,16 +1,30 @@
-function buildQueryUrl() {
-    var queryURL = "https://api.edamam.com/search?";
-    var appKey = "da6f303557b9ecd12d0710df3c225180";
-    var appID = "16ae349c";
-    var search = "q=" + ;
-    var quantity = "";
+// function buildQueryUrl(input) {
+//     var queryURL = "https://api.edamam.com/search?";
+//     var appKey = "&app_key=${da6f303557b9ecd12d0710df3c225180}";
+//     var appID = "&app_id=${16ae349c}";
+//     var search = "q=";
+//     var quantity = "&quantity=5";
 
-    var fullQuery = queryURL + search + appID + appKey + quantity;
+//     var fullQuery = queryURL + search + input + appID + appKey + quantity
 
-    return fullQuery
+//     return fullQuery
 
-}
+// }
 
-var input = $("#user-input");
-console.log(input);
-// id = "user-input" for the q =
+
+
+
+$(".btn").on("click", function (event) {
+    event.preventDefault();
+    var input = $("#user-input").val().trim();
+    console.log("clicked");
+    console.log(input);
+
+    // $.ajax({
+    //     url: buildQueryUrl(input),
+    //     method: "GET"
+    // }).then(function (response) {
+    //     console.log(response);
+    // });
+});
+
